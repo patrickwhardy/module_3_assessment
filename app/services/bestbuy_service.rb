ee1fa8537b6fd79256ef91e7c891bea6ff914b9f
patrickwhardy <patrickwhardy@gmail.com>
2016-06-14 10:27:10 -0600

    service does my bidding

--- app/services/bestbuy_service.rb
+++ app/services/bestbuy_service.rb
@@ -4,6 +4,6 @@ class BestbuyService
   end
 
   def get_stores(zip)
-    JSON.parse(@connection.get("stores(area(#{zip},25))?format=json&show=storeId,storeType,name&pageSize=2&apiKey=#{ENV['best_buy_key']}").body)
+    JSON.parse(@connection.get("stores(area(#{zip},25))?format=json&show=longName,city,distance,phone&pageSize=15&apiKey=#{ENV['best_buy_key']}").body)
   end
 end
--- spec/services/bestbuy_service_spec.rb
+++ spec/services/bestbuy_service_spec.rb
@@ -8,10 +8,15 @@ RSpec.describe BestbuyService do
 
   it "gets stores by zip" do
     service = BestbuyService.new
-    stores = service.get_stores("80202")
-    binding.pry
-    expect(stores["total"]).to eq(17)
-    # count, And I should see the long name, city, distance, phone number
-    # and store type
+    stores_json = service.get_stores("80202")
+    expect(stores_json["total"]).to eq(17)
+    expect(stores_json["stores"].count).to eq(15)
+
+    store = stores_json["stores"].first
+
+    expect(store["longName"]).to eq("Best Buy Mobile - Cherry Creek Shopping Center")
+    expect(store["city"]).to eq("Denver")
+    expect(store["distance"]).to eq(3.25)
+    expect(store["phone"]).to eq("303-270-9189")
   end
 end
