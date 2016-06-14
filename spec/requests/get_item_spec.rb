4e368688f33a0868a57b4ee75330db7c98ec8b5f
patrickwhardy <patrickwhardy@gmail.com>
2016-06-14 09:37:22 -0600

    write get item test

---
+++ spec/requests/get_item_spec.rb
@@ -0,0 +1,28 @@
+# When I send a GET request to /api/v1/items/1
+# I receive a 200 JSON response containing the name, description,
+# and image_url but not the created_at or updated_at
+require "rails_helper"
+
+RSpec.describe "GET /api/v1/items/:id" do
+  it "returns an item with name, description and image_url" do
+    Item.create(
+      name: "Name",
+      description: "Description",
+      image_url: "Image"
+    )
+    expect(Item.count).to eq(1)
+    item = Item.first
+
+    get "/api/v1/items/#{item.id}.json"
+    expect(response.status).to eq 200
+
+    item_json = JSON.parse(response.body)
+
+    expect(item_json["id"]).to eq(item.id)
+    expect(item_json["name"]).to eq(item.name)
+    expect(item_json["description"]).to eq(item.description)
+    expect(item_json["image_url"]).to eq(item.image_url)
+
+    expect(item_json.keys).to eq(["id", "name", "description", "image_url"])
+  end
+end
--- spec/requests/get_items_spec.rb
+++ spec/requests/get_items_spec.rb
@@ -1,6 +1,3 @@
-# When I send a GET request to /api/v1/items I receive a 200 JSON
-# response containing all items And each item has a name, description,
-#  and image_url but not the created_at or updated_at
 require "rails_helper"
 
 RSpec.describe "GET /api/v1/items" do
